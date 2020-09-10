42e9879b5edeb7e75d2dfc4999b20f9b32fdfec8
Shashikant <shashikant.1997k@gmail.com>
2020-09-10 12:48:18 +0530

    Updates

---
+++ precache-manifest.ac11b93d2c697b0b8885288a928c2074.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78bee60fcf2406ae0b4a9bb6ec9bf3df",
+    "url": "/trip-summary/index.html"
+  },
+  {
+    "revision": "012e6b29e7108e20e915",
+    "url": "/trip-summary/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "6674d4ac48b49a20a5bd",
+    "url": "/trip-summary/static/css/main.474c69c4.chunk.css"
+  },
+  {
+    "revision": "012e6b29e7108e20e915",
+    "url": "/trip-summary/static/js/2.de0404ff.chunk.js"
+  },
+  {
+    "revision": "082c1bdf24861af2babbd4591bc978fa",
+    "url": "/trip-summary/static/js/2.de0404ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6674d4ac48b49a20a5bd",
+    "url": "/trip-summary/static/js/main.905cac09.chunk.js"
+  },
+  {
+    "revision": "cabd42d51c34cd3e70c6",
+    "url": "/trip-summary/static/js/runtime-main.3b85e8c1.js"
+  }
+]);
